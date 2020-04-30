7838544664525be911e379a8158bd933244f5992
oscarmorrison <github@oscarmorrison.com>
2020-04-30 16:17:34 +1000

    rm tag nav, update styling

--- assets/js/oscar.js
+++ assets/js/oscar.js
@@ -1,8 +1,5 @@
 let oscar = {
     animatedNav: true,
-    tagMenu: true,
-    tagKey: '#tagnav',
-    requirePosts: false
 };
 
 if(oscar.animatedNav) {
@@ -17,24 +14,5 @@ if(oscar.animatedNav) {
 $('a[href^=http]').each(function(){
     if(this.href.indexOf(location.hostname) < 0) {
         $(this).attr({target: '_blank'});
-    }
-});
-
-$(function() {
-    if(location.pathname === '/' && oscar.tagMenu) {
-        $.get(ghost.url.api('tags', {limit: 'all', include: 'count.posts', order: 'count.posts DESC'}))
-            .then(function(data) {
-                var navTags = data.tags.filter(function(tag){
-                    return (oscar.requirePosts
-                        ? tag.description && tag.count.posts && tag.description.includes(oscar.tagKey)
-                        : tag.description && tag.description.includes(oscar.tagKey)
-                    );
-                }).forEach(function(tag){
-                    $('<a>', {
-                        href: '/tag/'+tag.slug,
-                        text: tag.name
-                    }).prependTo('.tag-nav');
-                });
-            });
     }
 });
